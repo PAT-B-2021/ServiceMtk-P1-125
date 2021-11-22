091562e44fbab2edf8bca7edd115f7f92d3324b5
ridwannurfaizin12 <ridwannurfaizin12>
2021-11-22 23:31:19 +0700

    Nama : Ridwan Nur faizin NIM : 20190140125

--- Matematika.cs
+++ Matematika.cs
@@ -12,29 +12,102 @@ namespace ServiceMtk_P1_125
     {
         public int Bagi(int a, int b)
         {
-            return a / b;
+            try
+            {
+                return a / b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MathFault mf = new MathFault
+                {
+                    Kode = "Inputan yang anda masukkan salah",
+                    Pesan = "Masukkan input yang benar"
+                };
+                throw new FaultException<MathFault>(mf);
+            }
+
         }
         public int Kali(int a, int b)
         {
-            return a * b;
+            try
+            {
+                return a * b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MathFault mf = new MathFault
+                {
+                    Kode = "Inputan yang anda masukkan salah",
+                    Pesan = "Masukkan input yang benar"
+                };
+                throw new FaultException<MathFault>(mf);
+            }
+
         }
         public int Kurang(int a, int b)
         {
-            return a - b;
+            try
+            {
+                return a - b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MathFault mf = new MathFault
+                {
+                    Kode = "Inputan yang anda masukkan salah",
+                    Pesan = "Masukkan input yang benar"
+                };
+                throw new FaultException<MathFault>(mf);
+            }
+
         }
         public int Tambah(int a, int b)
         {
-            return a + b;
+            try
+            {
+                return a + b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MathFault mf = new MathFault
+                {
+                    Kode = "Inputan yang anda masukkan salah",
+                    Pesan = "Masukkan input yang benar"
+                };
+                throw new FaultException<MathFault>(mf);
+            }
+
         }
         //TKoordinat:nama methodnya, a,b = input seperti int.
         //Membuat koordinat hasil lalu menjumlahkan xa dengan xb, ya
         //dengan yb lalu dimasukkan ke hasil
-public Koordinat TKoordinat(Koordinat a, Koordinat b)
+        public Koordinat TKoordinat(Koordinat a, Koordinat b)
         {
-            Koordinat hasil = new Koordinat();
-            hasil.X = a.X + b.X;
-            hasil.Y = a.Y + b.Y;
-            return hasil;
+            try
+            {
+                Koordinat hasil = new Koordinat
+                {
+                    X = a.X + b.X,
+                    Y = a.Y + b.Y
+                };
+                return hasil;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MathFault mf = new MathFault
+                {
+                    Kode = "Inputan yang anda masukkan salah",
+                    Pesan = "Masukkan input yang benar"
+                };
+                throw new FaultException<MathFault>(mf);
+            }
+
         }
     }
+
 }
